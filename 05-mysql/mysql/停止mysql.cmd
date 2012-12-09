7560fe89b90d3c860b801c19982b33c8939fb184
zsl <zhushaolong@gmail.com>
2012-12-09 16:47:52 +0800

    ..

--- 05-mysql/mysql/停止mysql.cmd
+++
@@ -1,4 +0,0 @@
-net stop mysql5.0.90_hz
-mysql5.0.90_hz\bin\mysqld-nt.exe --remove mysql5.0.90_hz
-
-pause
--- 05-mysql/mysql/启动mysql.cmd
+++
@@ -1,4 +0,0 @@
-mysql5.0.90_hz\bin\mysqld-nt.exe --install mysql5.0.90_hz --defaults-file="%CD%\mysql5.0.90_hz\my.ini"
-net start mysql5.0.90_hz
-
-pause
